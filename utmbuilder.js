755c38f8794a3ddeee75635f79d6391f96c0e6fd
Sarah Drasner <sarah.drasner@gmail.com>
2020-05-02 10:42:46 -0600

    use localstorage

--- utmbuilder.js
+++ utmbuilder.js
@@ -1,37 +1,19 @@
 "use strict"
 
-function saveChanges(type) {
-  let inputs = document.getElementById("utmform").elements
-  let formtype = inputs[type]
-
-  if (!formtype) {
-    message("Error: No value specified")
-    return
-  }
-
-  // Save it using the Chrome extension storage API.
-  chrome.storage.sync.set({ type: formtype }, function() {
-    // Notify that we saved.
-    message("Settings saved")
-  })
-}
-
 function click(e) {
-  chrome.storage.local.set({ isPaused: false })
-  //window.localStorage.setItem('medium', 'twitter');
-  //chrome.storage.sync.set('medium', 'twitter')
-  //saveChanges('medium')
-
-  // let medium = chrome.storage.sync.get('isPaused')
-  // console.log(medium)
-
   chrome.tabs.executeScript({ file: "content.js" })
-  window.close()
+  //window.close()
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  var inputs = document.getElementById("utmform").elements
-  console.log(inputs)
+  var form = document.getElementById("utmform")
+  window.localStorage.setItem("medium", form.elements["medium"].value)
+
+  form.addEventListener("change", () => {
+    window.localStorage.setItem("medium", form.elements["medium"].value)
+    window.localStorage.setItem("name", form.elements["name"].value)
+    window.localStorage.setItem("team", form.elements["team"].value)
+  })
 
   let button = document.getElementById("submit")
   button.addEventListener("click", click)
